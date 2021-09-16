c0f767f0a58ecceca1f84ca5bea42006f2df85c7
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-16 22:13:07 +0530

    ADDED: handles exceptions for vaidateDate

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
@@ -14,6 +14,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import com.bridgelabz.hotelreservationsystem.HotelReservationException.ExceptionType;
+
 
 public class HotelReservationSystem {
 	
@@ -29,9 +31,17 @@ public class HotelReservationSystem {
 		
 	}
 	
-	public static boolean validateDate(String date) {
-		String dateRegex="^([0-9]{4})[\\-]((0[1-9])|1[012])[\\-]([012][0-9]|[3][01])$";
-		return Pattern.matches(dateRegex, date);
+	public static boolean validateDate(String date) throws HotelReservationException {
+		try {
+			String dateRegex="^([0-9]{4})[\\-]((0[1-9])|1[012])[\\-]([012][0-9]|[3][01])$";
+			if(Pattern.matches(dateRegex, date))
+				return true;
+			else
+				throw new HotelReservationException("Enter date in proper format", ExceptionType.DATE_INVALID);
+		}catch(NullPointerException e) {
+			throw new HotelReservationException("Enter date", ExceptionType.DATE_NULL); 
+		}
+		
 	}
 	
 	public static boolean validateCustomerType(String customerType) {
