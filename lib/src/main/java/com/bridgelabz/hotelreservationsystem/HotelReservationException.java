2ac2ca61d1e277251ba65fcc31814e6e87e7d7f3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-16 22:05:34 +0530

    ADDED: enum ExceptionType created

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationException.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationException.java
@@ -1,6 +1,18 @@
 package com.bridgelabz.hotelreservationsystem;
 
 public class HotelReservationException extends RuntimeException {
+		
+	enum ExceptionType{
+		DATE_NULL,
+		DATE_INVALID,
+		CUSTOMER_TYPE_NULL,
+		CUTOMER_TYPE_INVALID
+	}
 	
+	ExceptionType type;
 	
+	public HotelReservationException(String message, ExceptionType type) {
+		super(message);
+		this.type=type;
+	}
 }
